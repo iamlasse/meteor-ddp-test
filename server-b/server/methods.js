2e9971609ec71a86c996981aa6c6d6b9a3a0c01d
Lasse Larsen <lasse@bisontiger.com>
2015-06-06 14:24:23 -0700

    remove more console.log lines

--- server-b/server/methods.js
+++ server-b/server/methods.js
@@ -2,7 +2,7 @@ Meteor.methods({
 	serverDelete: function (id) {
 		var aItem = ServerAItems.findOne({_id: id});
 		if(aItem){
-			console.log(aItem)
+			// console.log(aItem)
 			ServerAItems.remove({_id: id});
 		}
 	},
@@ -10,7 +10,7 @@ Meteor.methods({
 
 		var aItem = ServerAItems.findOne({_id: item._id});
 		if(aItem){
-			console.log(aItem);
+			// console.log(aItem);
 			ServerAItems.update({_id: aItem._id}, {$set: {test: aItem.test}});
 		} else {
 			ServerAItems.insert(item);
